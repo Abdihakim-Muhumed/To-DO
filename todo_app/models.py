2de2dab21cee92130ecab9729184d7c865475b3e
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-09-08 14:29:02 +0300

    Completed CRUD processes for the app

--- todo_app/models.py
+++ todo_app/models.py
@@ -21,7 +21,7 @@ class ToDoItem(models.Model):
     description =  models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(default=one_week_hence)
-    toddo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE, null=False)
     
     def get_absolute_url(self):
         return reverse(
